cad988a30441f1d9c9d24c95bc9959844ca180a2
Samruddh Talaulikar <samruddh.talaulikar@kilowott.com>
2021-11-30 13:39:00 +0530

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿//Main File
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DemoTwo
+namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+
+            Class1.ShowOutput();
         }
+
+
     }
 }
